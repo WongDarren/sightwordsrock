066a632785a9885588da11f22e0b503f120e7073
Trevor Mullaney <mullaney@unlv.nevada.edu>
2019-10-08 17:49:21 -0700

    added teacherlogin view

--- SightWordsProject/Controllers/TeacherController.cs
+++ SightWordsProject/Controllers/TeacherController.cs
@@ -8,16 +8,11 @@ using SightWordsProject.Models;
 
 namespace SightWordsProject.Controllers
 {
-    public class StudentController : Controller
+    public class TeacherController : Controller
     {
         public IActionResult Login()
         {
-            return View("Views/Student/StudentLogin.cshtml");
+            return View("Views/Teacher/TeacherLogin.cshtml");
         }
-        public IActionResult StudentStages()
-        {
-            return View();
-        }
-
     }
 }
