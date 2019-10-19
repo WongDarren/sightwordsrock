992c6fa6bce39765ef641b6cd0b57e6e6e897c29
Trevor Mullaney <mullaney@unlv.nevada.edu>
2019-10-18 20:24:02 -0700

    create new student

--- SightWordsProject/Controllers/StudentController.cs
+++ SightWordsProject/Controllers/StudentController.cs
@@ -10,6 +10,8 @@ namespace SightWordsProject.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
         public IActionResult Login()
         {
             return View("Views/Student/StudentLogin.cshtml");
@@ -21,6 +23,42 @@ namespace SightWordsProject.Controllers
         public IActionResult Stages()
         {
             return View("Views/Student/StudentStages.cshtml");
+        }
+
+        [HttpGet]
+        public IActionResult CreateAccount()
+        {
+            return View("Views/Student/CreateAccount.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount(CreateAccountVM model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser 
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    School = model.School,
+                    UserName = model.Email, 
+                    Email = model.Email, 
+                    StudentCode = model.StudentCode,
+                };
+                var result = await userManager.CreateAsync(user,model.Password);
+
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("TeacherDashboard", "Teacher");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            
+            return View("TeacherDashboard",model);
         }
 
     }
