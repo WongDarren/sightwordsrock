b31e6380de43ddfdd32af3a1b6366d82927995c8
Trevor Mullaney <mullaney@unlv.nevada.edu>
2019-11-01 23:06:14 -0700

    added sight word data

--- SightWordsProject/Migrations/20191102011612_SightWordTable.Designer.cs
+++ SightWordsProject/Migrations/20191102011612_SightWordTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SightWordsProject.Models;
 
 namespace SightWordsProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191102011612_SightWordTable")]
+    partial class SightWordTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -208,7 +210,7 @@ namespace SightWordsProject.Migrations
 
                     b.HasKey("ID");
 
-                    b.ToTable("WordsList");
+                    b.ToTable("Words");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
